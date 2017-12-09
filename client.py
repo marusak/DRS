881bc6de1ff70c079befb5a56c949c1b0bd6201d
Matej Marusak <mmarusak@redhat.com>
2017-12-09 21:36:53 +0100

    Add client
    Signed-off-by: Matej Marusak <mmarusak@redhat.com>

--- client.py
+++ client.py
@@ -1,20 +1,28 @@
+import signal
 from bluetooth import BluetoothSocket, RFCOMM
 
 
 def parsa_data(data):
-    print (data)
-    #while ("{" in data and "}" in data):
-    #    print (data[data.find('{'):data.find('}')])
-    #    data = data[data.find('}'):]
+    while ("{" in data and "}" in data):
+        print (data[data.find('{'):data.find('}')+1])
+        data = data[data.find('}')+1:]
     return data
 
+def finish_up(signum, frame):
+    signal.signal(signal.SIGINT, original_sigint)
+    sock.close()
 
+original_sigint = signal.getsignal(signal.SIGINT)
+signal.signal(signal.SIGINT, finish_up)
 sock = BluetoothSocket(RFCOMM)
 sock.connect(("98:D3:32:30:AB:5C", 1))
 
-data = ""
-#while True:
-data += sock.recv(160)
-data = parsa_data(data)
+data = sock.recv(165)
+data = data[data.find('}')+1:]
+while True:
+    data += sock.recv(165)
+    data = parsa_data(data)
+
+print(data)
 
 sock.close()
